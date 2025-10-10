d0028dadb1036576ae0151596a3abb25ff206435
QIseven7-791 <1850923559@qq.com>
2025-10-10 19:00:40 +0800

    update

---
+++ EZPZ_Interaction_Toolkit-main/Assets/JUNYI QI9912/QJY9912ASSESSMENT/Script/keyboard.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class keyboard : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 startPosition;  
+    public Vector3 sinOffset;      
+    public float alpha;            
+    public float sinValue;         
+    public float rangeFactor;      
+    public float keySpeed;         
+
+    void Start()
+    {
+        startPosition = target.localPosition;
+        alpha = 0f;
+    }
+
+    void Update()
+    {
+        if (alpha >= 180f)
+        {
+            target.localPosition = startPosition; 
+            enabled = false;
+            alpha = 0f;
+            sinOffset = Vector3.zero;
+           
+            gameObject.SetActive(false);
+            return;
+        }
+        sinValue = -Mathf.Abs(Mathf.Sin(alpha * Mathf.Deg2Rad));      
+        sinOffset.y = sinValue * rangeFactor;
+        target.localPosition = startPosition + sinOffset;
+        alpha += keySpeed * Time.deltaTime;
+
+      
+
+    }
+}
+
